a51ae41467b580d56eea9289068f806fc38df5b5
Nikita Baruah <nikita_baruah27@milton.edu>
2025-11-05 22:03:54 -0500

    Finished question number 1

--- index.js
+++ index.js
@@ -2,22 +2,42 @@ import data from './rawBooks.json' with { type: 'json' };
 console.log(data[57]['# Pages']);
 
 data.forEach(book=>{
-    console.log(book['read?'])
+    //console.log(book['read?'])
+    //console.log(data.avgTime);
+
 });
 
-for (let i=0; i<book.length; i++){
+
+const avgTime= data.filter(myTime);
+function myTime(book){
+    return book['read?']=="Yes" && book['Time on TBR (days)']!==121.5863014 && book['Time on TBR (years)']!==-121.5863014 && book['Time on TBR (days)']!=="N/A";
+}
+
+const avgDays= avgTime.reduce((sum,book)=>sum+book['Time on TBR (days)'],0)/avgTime.length;
+
+/*avgTime.forEach(book => {
+  console.log(book['Time on TBR (days)'], typeof book['Time on TBR (days)']);
+});
+
+
+//console.log(avgDays);
+
+
+/*for (let i=0; i<book.length; i++){
     sum+= book[i]['Time on TBR (days)'];
 }
 
-function avg(time){
-    return sum/
+function avg(value){
+    return sum/value.length;
 }
 
-if(book['read?']=="Yes"){
-    for (let i=0; i<book.length; i++){
-        sum+= book[i]['Time on TBR (days)'];
-    }
+if(book.myTime){
+    if(book['Time on TBR (years)']=-121.5863014||book['Time on TBR (years)']==121.5863014){
 
+    }
+    else{
+        console.log(book['Time on TBR (days)'].avg);
+    }
 }
 
 /*avg time it takes me to read a book
